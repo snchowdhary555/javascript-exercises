d69f949becc288c49892bf5e95ea2d04ce413010
snchowdhary555 <snchowdhary555@gmail.com>
2022-05-07 18:52:35 +0530

    Complete the exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+  let sum = 0;
+  if (
+    min < 0 ||
+    max < 0 ||
+    typeof min !== "number" ||
+    typeof max !== "number"
+  ) {
+    return "ERROR";
+  }
+  if (min > max) {
+    let temp = max;
+    max = min;
+    min = temp;
+  }
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
