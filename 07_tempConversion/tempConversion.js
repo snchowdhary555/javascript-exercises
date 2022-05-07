bee4523c77692fb0286266369276f35e45a1f9ca
snchowdhary555 <snchowdhary555@gmail.com>
2022-05-07 19:30:05 +0530

    Complete the exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (fTemp) {
+  const cTemp = ((fTemp - 32) * 5) / 9;
+  return +cTemp.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function (cTemp) {
+  const fTemp = (cTemp * 9) / 5 + 32;
+  return +fTemp.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
