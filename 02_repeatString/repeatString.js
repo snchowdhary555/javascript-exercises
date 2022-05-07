4af0acb711c4c8836da49aa9a5ef9d2697414c59
snchowdhary555 <snchowdhary555@gmail.com>
2022-05-07 16:13:15 +0530

    Complete the exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, times) {
+  if (times < 0) return "ERROR";
+  let repeatedStr = "";
+  for (let i = 1; i <= times; i++) {
+    repeatedStr += str;
+  }
+  return repeatedStr;
 };
 
 // Do not edit below this line
